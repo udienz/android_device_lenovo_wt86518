eabb313ff64c0ab7ad384cb130782b1c10cab439
Mahyuddin Susanto <udienz@gmail.com>
2016-02-18 04:19:09 -0800

    Disabled 32-bit arch
     - BoardConfig.mk: Disable 32-arch
     - device.mk: include gapps when build
     - cm.dependencies: added
    
    Change-Id: I6f934ac736c082c0fce97e9981bd7733bcfbc14c
    Signed-off-by: Mahyuddin Susanto <udienz@gmail.com>

--- device.mk
+++ device.mk
@@ -237,3 +237,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
+
+GAPPS_VARIANT := micro
