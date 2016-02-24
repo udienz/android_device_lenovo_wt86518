0b2f30fbddafdf7e4ac58a71ba7d8abe9f9009c9
Mahyuddin Susanto <udienz@gmail.com>
2016-02-24 11:02:57 -0800

    Moving local config to local.mk
     - Add local.mk, you may add private packages (gapps, credential, etc)
       at here. This file will not add in git repo as describe at .gitignore
     - BoardConfig.mk: build with telo kernel
     - device.mk: call local.mk if exist
     - rootdir/etc/hosts: add ada blocker

--- device.mk
+++ device.mk
@@ -14,7 +14,8 @@
 # limitations under the License.
 #
 $(call inherit-product-if-exists, vendor/lenovo/wt86518/wt86518-vendor.mk)
-
+$(call inherit-product-if-exists, device/lenovo/wt86518/local.mk)
+i
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
@@ -238,5 +239,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
 
-GAPPS_VARIANT := micro
-$(call inherit-product, vendor/google/build/opengapps-packages.mk)
